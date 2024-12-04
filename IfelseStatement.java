cd74db65238dce22ad522d8e4dd3652164943dfe
RamyaM2005 <ramyareddym023@gmail.com>
2024-12-04 10:54:41 +0530

    Add files via upload

---
+++ IfelseStatement.java
@@ -0,0 +1,14 @@
+public class IfelseStatement {
+    public static void main(String args[])
+    {
+        int a=30,b=40;
+        if(a>b)
+        {
+            System.out.println("if block executed");
+        }
+        else{
+            System.out.println("else block executed");
+        }
+    }
+
+}
