47010cc27e7dc1745114a942d2069e211bc68354
RamyaM2005 <ramyareddym023@gmail.com>
2024-12-04 10:16:38 +0530

    Add files via upload

---
+++ IfStatement.java
@@ -0,0 +1,11 @@
+public class IfStatement {
+    public static void main(String args[])
+    {
+        int a=3;
+        if(a>0)
+        {
+            System.out.println("if block executed");
+        }
+    }
+    
+}
