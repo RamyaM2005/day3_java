0cacc0867f1dbd8a3af990e747ed0aa65471e605
RamyaM2005 <ramyareddym023@gmail.com>
2024-12-05 22:30:53 +0530

    Add files via upload

---
+++ Multiple.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Multiple
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        if((n&3)==0)
+        {
+        System.out.println("Multiple of 4");
+        }
+        else
+        {
+        System.out.println("Not a Multiple of 4");
+        }
+    }    
+}
